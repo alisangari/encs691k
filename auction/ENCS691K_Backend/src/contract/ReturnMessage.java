8346a5f745aa83ab763b00bea0f8d4b44f0d525f
Ali Sangari <a_b_sangari@yahoo.com>
2015-11-02 07:02:17 -0500

    message

---
+++ auction/ENCS691K_Backend/src/contract/ReturnMessage.java
@@ -0,0 +1,28 @@
+package contract;
+
+public class ReturnMessage {
+
+	private int code;
+	private String message;
+	
+	public ReturnMessage(){
+		this.code = 0;
+		this.message = "Message not initialized.";
+	}
+	
+	public void ok(){
+		this.code = 1;
+		this.message = "Operation was successful.";
+	}
+	
+	public void duplicateExists(){
+		this.code = -1;
+		this.message = "Username already exists.";
+	}
+	
+	public void invalidUsernameOrPassword(){
+		this.code=-2;
+		this.message="Either username or password was invalid.";
+	}
+}
+
